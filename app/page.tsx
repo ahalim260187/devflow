19740d1ae48711325cb7c328d36f91193bbbca9b
=ahalim260187 <raffahaitami@gmail.com>
2024-10-27 20:08:53 +0800

    Add Tailwind Config for color, boxshadow and screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-black text-white ">
-      Welcome to teh world of next js
+    <h1 className="text-3xl font-black text-light-500 ">
+      Welcome to the world of next js
     </h1>
   );
 }
