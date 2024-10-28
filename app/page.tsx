84773841d9378f6b5f24c716e950ec58d86bb27d
ahalim260187 <raffahaitami@gmail.com>
2024-10-28 11:46:29 +0800

    Adding Tailwind css theming:

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-black text-light-500 ">
-      Welcome to the world of next js
-    </h1>
+    <h1 className="text-dark500_light500 ">Welcome to the world of next js</h1>
   );
 }
